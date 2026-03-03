b06e55c43c550b8488120cd03d57643853d6d870
fzxen <zx_fan@outlook.com>
2026-03-03 09:23:34 +0800

    feat: export macros

--- src/macros/export_property.hpp
+++ src/macros/export_property.hpp
@@ -11,38 +11,27 @@
 namespace godot
 {
 
-// 类型映射辅助宏
-#define _GET_VARIANT_TYPE(type) \
-  _GET_VARIANT_TYPE_##type
-
-#define _GET_VARIANT_TYPE_float Variant::FLOAT
-#define _GET_VARIANT_TYPE_int Variant::INT
-#define _GET_VARIANT_TYPE_bool Variant::BOOL
-#define _GET_VARIANT_TYPE_String Variant::STRING
-#define _GET_VARIANT_TYPE_Vector2 Variant::VECTOR2
-#define _GET_VARIANT_TYPE_Vector3 Variant::VECTOR3
-
 #define GD_BIND_GETTER_SETTER(CLASS_NAME, NAME)                                      \
   ClassDB::bind_method(D_METHOD("set_" #NAME, "p_" #NAME), &CLASS_NAME::set_##NAME); \
   ClassDB::bind_method(D_METHOD("get_" #NAME), &CLASS_NAME::get_##NAME);
 
-#define GD_PROPERTY(CLASS_NAME, TYPE, NAME, DEFAULT_VALUE, HINT, HINT_STRING)                                  \
-  WRITEABLE_PROPERTY(TYPE, NAME, DEFAULT_VALUE)                                                                \
-  static void _bind_##NAME##_property()                                                                        \
-  {                                                                                                            \
-    GD_BIND_GETTER_SETTER(CLASS_NAME, NAME)                                                                    \
-    ADD_PROPERTY(PropertyInfo(_GET_VARIANT_TYPE(TYPE), #NAME, HINT, HINT_STRING), "set_" #NAME, "get_" #NAME); \
+#define EXPORT_PROPERTY(CLASS_NAME, TYPE, NAME, DEFAULT_VALUE, VARIANT_TYPE, HINT, HINT_STRING)     \
+  WRITEABLE_PROPERTY(TYPE, NAME, DEFAULT_VALUE)                                                     \
+  static void _bind_##NAME##_property()                                                             \
+  {                                                                                                 \
+    GD_BIND_GETTER_SETTER(CLASS_NAME, NAME)                                                         \
+    ADD_PROPERTY(PropertyInfo(VARIANT_TYPE, #NAME, HINT, HINT_STRING), "set_" #NAME, "get_" #NAME); \
   }
 
-#define GD_NODE_PROPERTY(CLASS_NAME, TYPE, NAME, HINT, HINT_STRING)                                                                       \
-  WRITEABLE_PROPERTY(TYPE, NAME, nullptr)                                                                              \
-  static void _bind_##NAME##_property()                                                                                \
-  {                                                                                                                    \
-    GD_BIND_GETTER_SETTER(CLASS_NAME, NAME)                                                                            \
+#define EXPORT_NODE_PROPERTY(CLASS_NAME, TYPE, NAME, HINT, HINT_STRING)                                \
+  WRITEABLE_PROPERTY(TYPE, NAME, nullptr)                                                              \
+  static void _bind_##NAME##_property()                                                                \
+  {                                                                                                    \
+    GD_BIND_GETTER_SETTER(CLASS_NAME, NAME)                                                            \
     ADD_PROPERTY(PropertyInfo(Variant::OBJECT, #NAME, HINT, HINT_STRING), "set_" #NAME, "get_" #NAME); \
   }
 
-#define GD_PACKED_SCENE_PROPERTY(CLASS_NAME, NAME)                                                                              \
+#define EXPORT_PACKED_SCENE_PROPERTY(CLASS_NAME, NAME)                                                                          \
   WRITEABLE_PROPERTY(Ref<PackedScene>, NAME, nullptr)                                                                           \
   static void _bind_##NAME##_property()                                                                                         \
   {                                                                                                                             \
@@ -50,7 +39,15 @@ namespace godot
     ADD_PROPERTY(PropertyInfo(Variant::OBJECT, #NAME, PROPERTY_HINT_RESOURCE_TYPE, "PackedScene"), "set_" #NAME, "get_" #NAME); \
   }
 
-#define GD_ENUM_PROPERTY(CLASS_NAME, TYPE, NAME, DEFAULT_VALUE, HINT_STRING)                                      \
+#define EXPORT_ARRAY_PROPERTY(CLASS_NAME, ITEM_TYPE, NAME)                                                              \
+  WRITEABLE_PROPERTY(TypedArray<ITEM_TYPE>, NAME, {})                                                                   \
+  static void _bind_##NAME##_property()                                                                                 \
+  {                                                                                                                     \
+    GD_BIND_GETTER_SETTER(CLASS_NAME, NAME)                                                                             \
+    ADD_PROPERTY(PropertyInfo(Variant::ARRAY, #NAME, PROPERTY_HINT_ARRAY_TYPE, ITEM_TYPE), "set_" #NAME, "get_" #NAME); \
+  }
+
+#define EXPORT_ENUM_PROPERTY(CLASS_NAME, TYPE, NAME, DEFAULT_VALUE, HINT_STRING)                                  \
 private:                                                                                                          \
   TYPE NAME = DEFAULT_VALUE;                                                                                      \
   int _NAME = DEFAULT_VALUE;                                                                                      \
